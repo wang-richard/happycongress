7069f2024a310e295ce0152a95e50d9e40eafac2
Richard Wang <rtwrtw8@gmail.com>
2019-03-26 15:37:03 -0700

    Add files via upload

---
+++ press_releases/2014_close_elections/frank_guinta/frankGuintaScript.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 15:13:18 2019
+
+@author: Richard
+"""
+
+from bs4 import BeautifulSoup
+import requests
+from datetime import datetime
+from dateutil.parser import parse
+
+headers = requests.utils.default_headers()
+
+headers.update(
+        {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+        }
+)
+
+urlsList = []
+errorsList = []
+baseURL_1 = 'https://web.archive.org/web/20161208035921/https://guinta.house.gov/media-center/press-releases'
+targetURL = baseURL_1
+try:
+    response = requests.get(targetURL, headers=headers)
+    text = response.text
+    soup = BeautifulSoup(text, 'html5lib')
+    a = soup.findAll('h3',attrs={"class":"field-content"})
+    for link in a:
+        url = link.find('a').attrs['href']
+        url = url.strip()
+        pressURL = 'https://web.archive.org'+url
+        urlsList.append(pressURL)
+
+except:
+    errorsList.append(targetURL)
+    pass
+
+baseURL = 'https://web.archive.org/web/20161208144847/https://guinta.house.gov/media-center/press-releases?page='
+for counter in range(1, 21):
+    print "URL counter: "+str(counter)
+    targetURL = baseURL+str(counter)
+    try:
+        response = requests.get(targetURL, headers=headers)
+        text = response.text
+        soup = BeautifulSoup(text, 'html5lib')
+        a = soup.findAll('h3',attrs={"class":"field-content"})
+        for link in a:
+            url = link.find('a').attrs['href']
+            url = url.strip()
+            pressURL = 'https://web.archive.org'+url
+            urlsList.append(pressURL)
+    
+    except:
+        print "error has occurred at "+str(counter)
+        errorsList.append(targetURL)
+        pass
+
+counter = 0
+pressList = []
+for targetURL in urlsList:
+    print "Press Release counter: "+str(counter)
+    string = ''
+    try:
+        response = requests.get(targetURL, headers=headers)
+        text = response.text
+        soup = BeautifulSoup(text, 'html5lib')
+        
+        #Grabbing the date-stamp for press release:
+        a = soup.find('div', attrs={"class":"panel-pane pane-node-created"})
+        date = a.get_text()
+        date = date.strip()
+        date = parse(date)
+        date = str(date)
+        date = date[0:10]
+        date = datetime.strptime(date, "%Y-%m-%d").strftime("%m.%d.%Y")
+        
+        #Grabbing the text for press release:
+        a = soup.find('div',attrs={"class":"panel-pane pane-entity-field pane-node-body"})
+        
+        [x.extract() for x in a.findAll('script')]
+        [x.extract() for x in a.findAll('style')]
+        [x.extract() for x in a.findAll("span", {'class':'hidden'})]  
+        [x.extract() for x in a.findAll("div", {'class':'hidden'})]  
+        
+        string += a.get_text()
+            
+    except:
+        print "error has occurred with url: "+str(targetURL)
+        errorsList.append(targetURL)
+        pass
+
+    string = string.replace('\r', ' ').replace('\n', ' ').replace('\t',' ')
+    string = string.strip()
+    string = ' '.join(string.split())
+    pressList.append(date+'\t'+string)
+    counter += 1
+
+text_file = open("frankGuintaPress.txt", "w")
+for press in pressList:
+    text_file.write(press.encode('utf-8')+"\n")
+text_file.close()
+
+text_file = open("frankGuintaPressErrors.txt", "w")
+for error in errorsList:
+    text_file.write(error.encode('utf-8')+"\n")
+text_file.close()
