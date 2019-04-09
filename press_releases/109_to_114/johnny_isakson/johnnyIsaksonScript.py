0af2f6314e30e283e57bde78803d47b03ab94e10
Richard Wang <rtwrtw8@gmail.com>
2019-04-08 21:20:18 -0700

    Add files via upload

---
+++ press_releases/109_to_114/johnny_isakson/johnnyIsaksonScript.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 08 16:52:12 2019
+
+@author: Richard
+"""
+
+from bs4 import BeautifulSoup
+import requests
+from datetime import datetime
+from dateutil.parser import parse
+import re
+
+headers = requests.utils.default_headers()
+
+headers.update(
+        {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+        }
+)
+
+baseURL = 'https://www.isakson.senate.gov/public/index.cfm/news-releases?page='
+urlsList = []
+errorsList = []
+for counter in range(1, 209):
+    print "URL counter: "+str(counter)
+    targetURL = baseURL+str(counter)
+    try:
+        response = requests.get(targetURL, headers=headers)
+        text = response.text
+        soup = BeautifulSoup(text, 'html5lib')
+        a = soup.findAll('td',attrs={"class":"recordListTitle"})
+        for link in a:
+            url = link.find('a').attrs['href']
+            url = url.strip()
+            pressURL = url
+            urlsList.append(pressURL)
+    
+    except:
+        print "error has occurred at "+str(counter)
+        errorsList.append(targetURL)
+        pass
+
+counter = 0
+pressList = []
+for targetURL in urlsList:
+    print "Press Release counter: "+str(counter)
+    string = ''
+    try:
+        response = requests.get(targetURL, headers=headers)
+        text = response.text
+        soup = BeautifulSoup(text, 'html5lib')
+        
+        #Grabbing the date-stamp for press release:
+        month = soup.find('span', attrs={"class":"month"}).get_text()
+        day = soup.find('span', attrs={"class":"day"}).get_text()
+        year = soup.find('span', attrs={"class":"year"}).get_text()
+        date = month+" "+day+", "+year
+        date = re.search(r'\b(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|Dec(ember)?)\s+\d{1,2},\s+\d{4}', date).group()
+        date = parse(date)
+        date = str(date)
+        date = date[0:10]
+        date = datetime.strptime(date, "%Y-%m-%d").strftime("%m.%d.%Y")
+        
+        #Grabbing the text for press release:
+        a = soup.find('div',attrs={"class":"content"})
+                
+        [x.extract() for x in a.findAll('script')]
+        [x.extract() for x in a.findAll('style')]
+        [x.extract() for x in a.findAll("span", {'class':'hidden'})]  
+        [x.extract() for x in a.findAll("div", {'class':'hidden'})]  
+        
+        string += a.get_text()
+    
+    except:
+        print "error has occurred with url: "+str(targetURL)
+        errorsList.append(targetURL)
+        pass
+
+    string = string.replace('\r', ' ').replace('\n', ' ').replace('\t',' ')
+    string = string.strip()
+    string = ' '.join(string.split())
+    pressList.append(date+'\t'+string)
+    counter += 1
+
+text_file = open("johnnyIsaksonPress.txt", "w")
+for press in pressList:
+    text_file.write(press.encode('utf-8')+"\n")
+text_file.close()
+
+text_file = open("johnnyIsaksonPressErrors.txt", "w")
+for error in errorsList:
+    text_file.write(error.encode('utf-8')+"\n")
+text_file.close()
